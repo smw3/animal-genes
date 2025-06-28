ea26ab8f67cc3a98c9d91b219bc84bc0d079565f
Ingendum <mariuswalz1992@gmail.com>
2025-06-28 08:36:52 +0200

    Switched approach completely

--- Source/AnimalGenes/HarmonyPatches.cs
+++ Source/AnimalGenes/HarmonyPatches.cs
@@ -1,4 +1,6 @@
-﻿using HarmonyLib;
+﻿using BigAndSmall;
+using HarmonyLib;
+using RimWorld;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,56 +8,59 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using Verse;
+using static BigAndSmall.RomanceTags;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace AnimalGenes
 {
     [StaticConstructorOnStartup]
-    static class HarmonyPatches
+    public static class HarmonyPatches
     {
         static HarmonyPatches()
         {
+            Harmony.DEBUG = true; // Enable Harmony debug logging
             var harmony = new Harmony("ingendum.animalgenes");
+            Log.Message("Patching");
             harmony.PatchAll();
-        }
-    }
+            Log.Message("Patched");
 
-    public static class PawnRenderNodeExtensions
-    {
-        public static bool RenderWhileAnimalGeneActive(this PawnRenderNodeWorker nodeWorker)
-        {
-            return true;
-        }
-    }
-
-    [HarmonyPatch(typeof(PawnRenderNodeWorker), nameof(PawnRenderNodeWorker.CanDrawNow))]
-    class Patch01
-    {
-
-        static bool Postfix(bool result, PawnRenderNode node, PawnDrawParms parms)
-        {
-            if (!result) return false;
-            if (node == node.tree.rootNode) {
-                return result;
-            }
-
-            if (node == null) throw new ArgumentNullException(nameof(node));
-
-            if (node.GetType() != typeof(PawnRenderNode_ReplaceAsAnimal))
+            Log.Message("Setting romance tags for humanlike animals");
+            Log.Message($"Total humanlike animals: {HumanlikeAnimalGenerator.humanlikeAnimals.Count}");
+            // Iterate over all values in the dictionary HumanlikeAnimalGenerator.humanlikeAnimals
+            foreach (var kvp in HumanlikeAnimalGenerator.humanlikeAnimals)
             {
-                Pawn pawn = parms.pawn;
-                if (pawn == null || pawn.genes == null) return result;
+                ThingDef newThing = kvp.Value.humanlikeThing;
+                // Check if the animalThing is not null
+                if (newThing != null)
+                {
+                    RaceExtension rExt = newThing.modExtensions.Where(x => x is RaceExtension).FirstOrDefault() as RaceExtension;
+                    List<HediffDef> raceHediffs = rExt.RaceHediffs;
 
-                if (pawn.genes.Xenogenes.Where((Gene g) => g.Label == "animal kind").Any())
-                {
-                    return false;
-                }
-                if (pawn.genes.Endogenes.Where((Gene g) => g.Label == "animal kind").Any())
-                {
-                    return false;
+                    foreach(HediffDef hediff in raceHediffs)
+                    {
+                        PawnExtension pExt = hediff.GetAllPawnExtensionsOnHediff().FirstOrDefault();
+                        if (pExt != null)
+                        {
+                            pExt.romanceTags.compatibilities.Add("Humanlike", new Compatibility
+                            {
+                                chance = 0.75f,
+                                factor = 0.75f
+                            });
+                            pExt.romanceTags.compatibilities.Add("Human", new Compatibility
+                            {
+                                chance = 0.5f,
+                                factor = 0.5f
+                            });
+                            Log.Message($"Set romance tags for {newThing.defName}");
+                        }
+                    }
+
+                    newThing.SetStatBaseValue(RomancePatches.FlirtChanceDef, 1);
+                    newThing.SetStatBaseValue(StatDefOf.Fertility, 1);
+
+                    Log.Message($"Set flirt chance for {newThing.defName} to {newThing.GetStatValueAbstract(RomancePatches.FlirtChanceDef)}");
                 }
             }
-
-            return result;
         }
     }
 }
